ed5f29075f27c3abe4487d0f37c012f0aab52f53
zizizo007 <zizizo007@gmail.com>
2020-04-14 11:40:15 +0300

    Additions
    Still the initial work on the project

--- main_object2cluster.py
+++ main_object2cluster.py
@@ -18,20 +18,20 @@ class data_2_cluster(object):
     
     def __init__(self, path, typ=1):
         # when initializing the main object:
-        # a - the data and the normlized data (according to z-score) are added as an array
-        # b - the data and the normlized data are added as a dataframe
+        # X,X_norm - the data and the normlized data (according to z-score) are added as an array
+        # X_df,X_norm_df - the data and the normlized data are added as a dataframe
         
         #create empty arrays for the loaded data
         self.X      = []
         self.X_norm = []
         
-        voltages_a = []    
-        for voltage in np.arange(-2,2.01,0.01):
-            voltages_a.append(str(round(voltage,2)))
-    
-        
         #first, go over the source folder and load the data into a matrix X
-        if type==1:
+        if typ==1:
+            
+            self.voltages_a = []    
+            for voltage in np.arange(-2,2.01,0.01):
+                self.voltages_a.append(str(round(voltage,2)))
+            
             for root, dirs, files in os.walk(path):
                 for file in files:
                     if os.path.splitext(file)[1] == '.xlsx':
@@ -46,6 +46,28 @@ class data_2_cluster(object):
                                 self.X.append(np.asarray(df[df[column].name])) 
                                 # I(V) normalized
                                 self.X_norm.append(np.asarray(zscore(df[df[column].name])))
+            #convert the list to a numpy array
+            self.X = np.transpose(np.array(self.X))
+            self.X_norm = np.transpose(np.array(self.X_norm))
             #Convert the lists to a dataframe
-            self.X_df       = pd.DataFrame(np.asarray(self.X), columns=voltages_a)
-            self.X_norm_df  = pd.DataFrame(np.asarray(self.X_norm), columns=voltages_a)
+            self.X_df       = pd.DataFrame(self.X, index=self.voltages_a)
+            self.X_norm_df  = pd.DataFrame(self.X_norm, index=self.voltages_a)
+            print('New data_2_cluster object created\n=================================\n')
+            print('Object dimensions:\n')
+            print('Number of sample types: ' + str(self.X.shape[0])+'\n')
+            print('Number of samples taken: ' + str(self.X.shape[1])+'\n')
+        
+    def plot(self):
+        #This function plots the object attributes according to the user selection
+        selections = input('What would you like to plot?\nChoose from: X,X_norm\n')
+        selections = selections.split(',')
+        if 'X' in selections:
+            plt.figure(1)
+            print('Plotting the matrix X:\n')
+            for column in self.X.T:
+                plt.plot(self.voltages_a,self.X)
+        if 'X_norm' in selections:
+            plt.figure(2)
+            print('Plotting the normalized matrix X:\n')
+            for column in self.X_norm.T:
+                plt.plot(self.voltages_a,self.X_norm)
