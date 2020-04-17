9c79aad588a344d7987e54250aba6d31dbe3d548
zizizo007 <zizizo007@gmail.com>
2020-04-17 13:00:33 +0300

    Histogram & PCA
    1. PCA - added an option to choose between PCA on the data or on the histograms.
    Also, checks if there is a row of zeros before deciding if to use to covariance or correlation matrix.
    
    2. finished writing the function that creates the histograms, still need to figure out the best binning.

--- main_object2cluster.py
+++ main_object2cluster.py
@@ -14,12 +14,19 @@ import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+import seaborn as sns
+sns.set_context("talk")
+sns.set_style("white")
+
 class data_2_cluster(object):
     
     def __init__(self, path, typ=1):
         # when initializing the main object:
         # X,X_norm - the data and the normlized data (according to z-score) are added as an array
         # X_df,X_norm_df - the data and the normlized data are added as a dataframe
+        
+        #Define a name for the data set (user can override)
+        self.name = ''
         
         #create empty arrays for the loaded data
         self.X      = []
@@ -59,36 +66,93 @@ class data_2_cluster(object):
         
     def plot(self):
         #This function plots the object attributes according to the user selection
-        selections = input('What would you like to plot?\nChoose from: X,X_norm,X_shifted,X_histograms\n')
+        selections = input('What would you like to plot?\nChoose from: X,X_norm,X_shifted,X_histograms,PCA\n')
         selections = selections.split(',')
         if 'X' in selections:
             plt.figure(1)
             print('Plotting the matrix X:\n')
             for column in self.X.T:
                 plt.plot(self.voltages_a,column)
+            plt.title('Data Curves (Raw) - ' + self.name)
+            plt.xlabel('Voltage [V]')
+            plt.ylabel('Current [A]')
+            plt.tight_layout()
         if 'X_norm' in selections:
             plt.figure(2)
             print('Plotting the normalized matrix X:\n')
             for column in self.X_norm.T:
                 plt.plot(self.voltages_a,column)
+            plt.title('Data Curves (normalized) - ' + self.name)
+            plt.xlabel('Voltage [V]')
+            plt.ylabel('Normalized Current')
+            plt.tight_layout()
         if 'X_shifted' in selections:
             plt.figure(3)
             print('Plotting the normalized shifted matrix X:\n')
             for column in self.X_shifted.T:
                 plt.plot(self.voltages_a,column)
+            plt.title('Data Shifted to Zero Means - ' + self.name)
         if 'X_histograms' in selections:
             plt.figure(4)
             print('Plotting the histograms of the data')
+            ax1 = plt.subplot2grid((1,2), (0,0))
+            ax2 = plt.subplot2grid((1,2), (0,1), sharey=ax1)
+            for column in self.X_norm.T:
+                ax1.plot(self.voltages_a, column)
+                ax1.set_xlabel('Voltage [V]')
+                ax1.set_ylabel('Normalized Current')
             for column in self.X_hist.T:
-                plt.plot(self.bins, column)
-    
+                ax2.plot(column, self.bins, 'o--')
+                ax1.set_xlabel('Counts')
+            plt.title('Data Histograms - ' + self.name)
+            plt.tight_layout()
+        if 'PCA' in selections:
+            typ = input('Which PCA would you like to plot?\nChoose from: data, data_histograms\n')
+            typ = typ.split(',')
+            PC_count = input('How many PCs would you like to plot?\n')
+            PC_count = int(PC_count)
+            plt.figure(5)
+            if 'data' in typ: 
+                ax1 = plt.subplot2grid((2,1), (0,0))
+                ax2 = plt.subplot2grid((2,1), (1,0))
+                
+                for n in range(PC_count):
+                    ax1.plot(self.voltages_a, self.PCs[:,n], label=n)
+                ax1.set_xlabel('Voltage [V]')
+                ax1.set_ylabel('Current [?]')
+                ax1.legend(loc=0)
+                ax2.scatter(self.samples_PC_space[0,:], self.samples_PC_space[1,:] )
+                ax2.set_xlabel('PC #1 (' + str(round(self.v[0]*100,2)) + ' %)')
+                ax2.set_ylabel('PC #2 (' + str(round(self.v[1]*100,2)) + ' %)')
+                ax1.set_title('PCA Results - ' + self.name + ' data') 
+                plt.tight_layout()     
+            if 'data_histograms' in typ:
+                ax1 = plt.subplot2grid((2,1), (0,0))
+                ax2 = plt.subplot2grid((2,1), (1,0))
+                
+                for n in range(PC_count):
+                    ax1.plot(self.bins, self.PCs[:,n], label=n)
+                ax1.set_xlabel('Bin #')
+                ax1.set_ylabel('[?]')
+                ax1.legend(loc=0)
+                ax2.scatter(self.samples_PC_space[0,:], self.samples_PC_space[1,:] )
+                ax2.set_xlabel('PC #1 (' + str(round(self.v[0]*100,2)) + ' %)')
+                ax2.set_ylabel('PC #2 (' + str(round(self.v[1]*100,2)) + ' %)')
+                ax1.set_title('PCA Results - ' + self.name + ' data_histograms') 
+                plt.tight_layout()     
+                
+                               
     def pca(self, norm=True, shiftData=False):
         #this function calculaation the principal components of the data
         
-        if norm:
+        typ = input('Which data would you like to calculate PCA for?\nChoose from: data,data_norm,data_histograms\n')
+        
+        if typ == 'data_norm':
             data = self.X_norm
-        else:
+        if typ == 'data':
             data = self.X
+        if typ == 'data_histograms':
+            data = self.X_hist
         
         [M,N] = data.shape
         #If needed, substract the mean of each dimension (=sample type)
@@ -96,8 +160,12 @@ class data_2_cluster(object):
             data_mean = np.mean(data,1)
             data = data - np.tile(data_mean, (N,1)).T
             self.X_shifted = data
-        #calculate the coveriance matrix
-        self.data_covariance = np.corrcoef(data)
+        #calculate the coveriance/correlation matrix
+        # In order to avoid zero standard deviation check before if there are rows of zero
+        if True in (data == 0).all(axis=1):
+            self.data_covariance = np.cov(data)
+        else: #if there are not rows of zero, than there will be no dividing by zero
+            self.data_covariance = np.corrcoef(data)
         #find the eigenvalues and eigenvectors
         #(using np.linalg.eigh function which assumes a real and symettric matrix)
         [self.v,self.PCs] = np.linalg.eigh(self.data_covariance)
@@ -113,7 +181,7 @@ class data_2_cluster(object):
         print('Variance of the first 5 principal components:\n')
         print(self.v[0:4])
     
-    def create_hist(self, norm=True, bns_ratio=8):
+    def create_hist(self, norm=True):
         #This function runs over each curve (column in the data)
         #and computes a histogram
         
@@ -124,13 +192,14 @@ class data_2_cluster(object):
         
         #first, create a vector with the sum of all histograms
         #then, use the bins for the indicidual curves
-        self.X_allHistograms, bns = np.histogram(data, bins='fd')
+        self.X_allHistograms, bns = np.histogram(data, bins='auto')
         
-        #Calculate the histogram
+        #Calculate the histograms
         X_hist = []
         for column in data.T:
             hist, bin_edges = np.histogram(column, bins=bns) #the number of bins is the legnth of the vector divided by bns
             X_hist.append(hist)
+        self.bins = bin_edges[:-1] + np.diff(bin_edges) / 2 #get the bins centers for plotting
         self.X_hist = np.transpose(np.array(X_hist)) #convert to a numpy array
         
         print('Finished computing the histogram\n')
