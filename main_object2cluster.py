aaf2741c9cd3ee3876e99db4d48a2f802dfc5f1c
zizizo007 <zizizo007@gmail.com>
2020-04-18 21:51:09 +0300

    PCA for histogram done
    finished writing the create_hist function after adding kernel density estimation which solved the binning problem.

--- main_object2cluster.py
+++ main_object2cluster.py
@@ -13,9 +13,10 @@ from scipy.stats import zscore
 import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KernelDensity
 
 import seaborn as sns
-sns.set_context("talk")
+sns.set_context("paper")
 sns.set_style("white")
 
 class data_2_cluster(object):
@@ -93,21 +94,27 @@ class data_2_cluster(object):
                 plt.plot(self.voltages_a,column)
             plt.title('Data Shifted to Zero Means - ' + self.name)
         if 'X_histograms' in selections:
-            plt.figure(4)
+            f = plt.figure(4)
             print('Plotting the histograms of the data')
-            ax1 = plt.subplot2grid((1,2), (0,0))
-            ax2 = plt.subplot2grid((1,2), (0,1), sharey=ax1)
+            ax1 = plt.subplot2grid((1,3), (0,0))
+            ax2 = plt.subplot2grid((1,3), (0,1), sharey=ax1)
+            ax3 = plt.subplot2grid((1,3), (0,2), sharey=ax1)
             for column in self.X_norm.T:
                 ax1.plot(self.voltages_a, column)
-                ax1.set_xlabel('Voltage [V]')
-                ax1.set_ylabel('Normalized Current')
+                ax1.set_xlabel('Voltage [V]', fontsize=12)
+                ax1.set_ylabel('Normalized Current', fontsize=12)
             for column in self.X_hist.T:
                 ax2.plot(column, self.bins, 'o--')
-                ax1.set_xlabel('Counts')
-            plt.title('Data Histograms - ' + self.name)
-            plt.tight_layout()
+                ax2.set_xlabel('Probability Density', fontsize=12)
+                ax2.set_yticks([])
+            for column in self.X_KDE.T:
+                ax3.plot(column, self.bins_KDE, '--')
+                ax3.set_xlabel('Probability Density', fontsize=12)
+                ax3.set_yticks([])
+            f.suptitle('Data Histograms & KDE - ' + self.name, y=0.98)
+            f.subplots_adjust(wspace=None, hspace=None, top=0.92)
         if 'PCA' in selections:
-            typ = input('Which PCA would you like to plot?\nChoose from: data, data_histograms\n')
+            typ = input('Which PCA would you like to plot?\nChoose from: data, data_histograms, data_KDE\n')
             typ = typ.split(',')
             PC_count = input('How many PCs would you like to plot?\n')
             PC_count = int(PC_count)
@@ -139,13 +146,28 @@ class data_2_cluster(object):
                 ax2.set_xlabel('PC #1 (' + str(round(self.v[0]*100,2)) + ' %)')
                 ax2.set_ylabel('PC #2 (' + str(round(self.v[1]*100,2)) + ' %)')
                 ax1.set_title('PCA Results - ' + self.name + ' data_histograms') 
-                plt.tight_layout()     
+                plt.tight_layout() 
+            if 'data_KDE' in typ:
+                ax1 = plt.subplot2grid((2,1), (0,0))
+                ax2 = plt.subplot2grid((2,1), (1,0))
+                
+                for n in range(PC_count):
+                    ax1.plot(self.bins_KDE, self.PCs[:,n], label=n)
+                ax1.set_xlabel('Bin #')
+                ax1.set_ylabel('[?]')
+                ax1.legend(loc=0)
+                ax2.scatter(self.samples_PC_space[0,:], self.samples_PC_space[1,:] )
+                ax2.set_xlabel('PC #1 (' + str(round(self.v[0]*100,2)) + ' %)')
+                ax2.set_ylabel('PC #2 (' + str(round(self.v[1]*100,2)) + ' %)')
+                ax1.set_title('PCA Results - ' + self.name + ' data_KDE') 
+                plt.tight_layout() 
+            
                 
                                
-    def pca(self, norm=True, shiftData=False):
+    def pca(self, shiftData=False):
         #this function calculaation the principal components of the data
         
-        typ = input('Which data would you like to calculate PCA for?\nChoose from: data,data_norm,data_histograms\n')
+        typ = input('Which data would you like to calculate PCA for?\nChoose from: data,data_norm,data_histograms,data_KDE\n')
         
         if typ == 'data_norm':
             data = self.X_norm
@@ -153,6 +175,8 @@ class data_2_cluster(object):
             data = self.X
         if typ == 'data_histograms':
             data = self.X_hist
+        if typ == 'data_KDE':
+            data =self.X_KDE
         
         [M,N] = data.shape
         #If needed, substract the mean of each dimension (=sample type)
@@ -163,20 +187,40 @@ class data_2_cluster(object):
         #calculate the coveriance/correlation matrix
         # In order to avoid zero standard deviation check before if there are rows of zero
         if True in (data == 0).all(axis=1):
-            self.data_covariance = np.cov(data)
+            data_covariance = np.cov(data)
         else: #if there are not rows of zero, than there will be no dividing by zero
-            self.data_covariance = np.corrcoef(data)
+            data_covariance = np.corrcoef(data)
         #find the eigenvalues and eigenvectors
         #(using np.linalg.eigh function which assumes a real and symettric matrix)
-        [self.v,self.PCs] = np.linalg.eigh(self.data_covariance)
+        [v,PCs] = np.linalg.eigh(data_covariance)
         #normalizing the eigenvmaxectors to be in precntage
-        self.v = self.v / np.sum(self.v)
+        v = v / np.sum(v)
         #sort the values in decreasing order
-        sorted_indexexs = np.argsort(-self.v) #get the indexes first
-        self.v   = self.v[sorted_indexexs]
-        self.PCs[:] = self.PCs[:,sorted_indexexs] 
+        sorted_indexexs = np.argsort(-v) #get the indexes first
+        v   = v[sorted_indexexs]
+        PCs[:] = PCs[:,sorted_indexexs] 
         #project the data set on the PCs space
-        self.samples_PC_space = np.matmul(self.PCs.T , data)
+        samples_PC_space = np.matmul(PCs.T , data)
+        if typ == 'data_norm':
+            self.X_norm_covariance       = data_covariance
+            self.X_norm_v                = v
+            self.X_norm_PCs              = PCs
+            self.X_norm_samples_PC_space = samples_PC_space
+        if typ == 'data':
+            self.X_covariance       = data_covariance
+            self.X_v                = v
+            self.X_PCs              = PCs
+            self.X_samples_PC_space = samples_PC_space
+        if typ == 'data_histograms':
+            self.X_hist_covariance       = data_covariance
+            self.X_hist_v                = v
+            self.X_hist_PCs              = PCs
+            self.X_hist_samples_PC_space = samples_PC_space
+        if typ == 'data_KDE':
+            self.X_KDE_covariance       = data_covariance
+            self.X_KDE_v                = v
+            self.X_KDE_PCs              = PCs
+            self.X_KDE_samples_PC_space = samples_PC_space
         print('Finished computing PCA\n========================\n')
         print('Variance of the first 5 principal components:\n')
         print(self.v[0:4])
@@ -192,16 +236,30 @@ class data_2_cluster(object):
         
         #first, create a vector with the sum of all histograms
         #then, use the bins for the indicidual curves
-        self.X_allHistograms, bns = np.histogram(data, bins=len(data[:,0])*20)
+        self.X_allHistograms, bns = np.histogram(data, bins='auto', density=True)
+        self.bins_KDE = np.linspace(np.min(data), np.max(data), len(bns)*2)
+        #Calculate the histograms and KDE
         
-        #Calculate the histograms
         X_hist = []
+        X_KDE  = []
         for column in data.T:
-            hist, bin_edges = np.histogram(column, bins=bns) #the number of bins is the legnth of the vector divided by bns
+            hist, bin_edges = np.histogram(column, bins=bns, density=True) #the number of bins is the legnth of the vector divided by bns
             X_hist.append(hist)
-        self.bins = bin_edges[:-1] + np.diff(bin_edges) / 2 #get the bins centers for plotting
-        self.X_hist = np.transpose(np.array(X_hist)) #convert to a numpy array
         
-        print('Finished computing the histogram\n')
-        print('Bins range:')
+            #Calculate the kernel density estimation
+            # instantiate and fit the KDE model
+            bndwdth = (np.max(data)-np.min(data))/(150)
+            kde = KernelDensity(bandwidth=bndwdth, kernel='gaussian')
+            kde.fit(column[:, None])
+            # score_samples returns the log of the probability density
+            logprob = kde.score_samples(self.bins_KDE[:, None])
+            prob = np.exp(logprob)
+            X_KDE.append(prob)
+            
+        self.bins   = bin_edges[:-1] + np.diff(bin_edges) / 2 #get the bins centers for plotting
+        self.X_hist = np.transpose(np.array(X_hist)) #convert to a numpy array
+        self.X_KDE  = np.transpose(np.array(X_KDE)) #convert to a numpy array
+        
+        print('Finished computing the histogram and KDE\n')
+        print('Range:')
         print([np.min(data),np.max(data)])
