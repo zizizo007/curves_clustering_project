641f20a745fb65d15328f8b99119ef582abbe11e
zizizo007 <zizizo007@gmail.com>
2020-05-14 08:24:12 +0300

    merge class added
    Allows combining several data sets to a single data set for PCA. At this stage this is done only for AFM data

--- main_object2cluster.py
+++ main_object2cluster.py
@@ -159,13 +159,16 @@ class data_2_cluster(object):
             ax2 = plt.subplot2grid((2,1), (1,0))
                 
             for n in range(PC_count):
-                ax1.plot(xd, PCs[:,n], label=n)
+                ax1.plot(xd, PCs[:,n], label='PC #' + str(n+1))
                 ax1.set_xlabel(xlabel)
                 ax1.set_ylabel(ylabel)
-                ax1.legend(loc=0)
-                ax2.scatter(samples_PC_space[0,:], samples_PC_space[1,:] )
+                ax1.legend(loc='upper right')
+                ax1.set_xlim([xd[0],xd[-1]])
+                ax2.scatter(samples_PC_space[0,:], samples_PC_space[1,:] , edgecolors='k', alpha=0.2)
                 ax2.set_xlabel('PC #1 (' + str(round(v[0]*100,2)) + ' %)')
                 ax2.set_ylabel('PC #2 (' + str(round(v[1]*100,2)) + ' %)')
+                ax2.set_xlim([np.min(samples_PC_space[0,:]) , np.max(samples_PC_space[0,:])])
+                ax2.set_ylim([np.min(samples_PC_space[1,:]) , np.max(samples_PC_space[1,:])])
                 ax1.set_title('PCA Results - ' + self.name + ' data') 
                 plt.tight_layout()
         
@@ -219,7 +222,7 @@ class data_2_cluster(object):
             [M,N] = self.X_ofInterest.shape
             sns.set_palette(sns.color_palette("coolwarm", N), N)
             for column in self.X_ofInterest.T:
-                plt.plot(self.x_modified, column)
+                plt.plot(self.x_modified, column, lw=0.4)
             plt.xlim(self.x_modified[0],self.x_modified[-1])
             plt.title('All Force Curves')
         if 'PCA' in selections:
@@ -390,15 +393,174 @@ class data_2_cluster(object):
 
 class merge_data(object):
     def __init__(self, data_sets):
+        typ = input('Which data type would you like to merge?\nChoose from: data,data_norm,data_histograms,data_KDE,data_AFM\n')
         
-        self.X_merged=False
-        self.X_norm_merged=False
-        former_X_set = data_sets[0].X
-        former_Xnorm_set = data_sets[0].X_norm
-        for data_set in data_sets:
-            if self.X_merged == False:
-                self.X_merged = former_X_set
-                self.X_norm_merged = former_Xnorm_set
-            else:
-                self.X_merged = np.append(self.X_merged, data_set.X)
-                self.X_norm_merged = np.append(self.X_norm_merged, data_set.X_norm)
+        #merge the data into a single matrix by using the append function
+        sets_lengths = []
+        if typ == 'data_AFM':
+            self.X_AFM_merged = []
+            former_X_AFM = data_sets[0].X_ofInterest
+            for data_set in data_sets:
+                sets_lengths.append(data_set.X_ofInterest.shape[1])
+                if len(self.X_AFM_merged) == 0:
+                    self.X_AFM_merged = former_X_AFM
+                else:
+                    self.X_AFM_merged = np.append(self.X_AFM_merged, data_set.X_ofInterest, axis=1)
+        
+        #get the indexes of the merged data sets
+        for i in range(len(sets_lengths)):
+            if i==0:
+                sets_lengths[i] = np.sum(sets_lengths[0:1])
+            else:    
+                sets_lengths[i] = np.sum(sets_lengths[i-1:i+1])
+        self.datas_length = sets_lengths
+        
+        #save the object in an array as the new object attribute
+        self.all_objects = data_sets
+        
+    def plot(self):
+        #This function plots the object attributes according to the user selection
+        def setupPalette(count, pal=None):
+            # See http://xkcd.com/color/rgb/. These were chosen to be different "enough".
+            colors = ['windows blue' , 'amber' , 'faded green' , 'dusty purple' , 'pale red',
+                      'grass green', 'dirty pink', 'azure', 'tangerine', 'strawberry',
+                      'yellowish green', 'gold', 'sea blue', 'lavender', 'orange brown', 'turquoise',
+                      'royal blue', 'cranberry', 'pea green', 'vermillion', 'sandy yellow', 'greyish brown',
+                      'magenta', 'silver', 'ivory', 'carolina blue', 'very light brown']
+        
+            palette = sns.color_palette(palette=pal, n_colors=count) if pal else sns.xkcd_palette(colors)
+            sns.set_palette(palette, n_colors=count)
+
+        setupPalette(20)
+        
+        def plot_pca(xd, PC_count, PCs, samples_PC_space, v, xlabel, ylabel, datas_lengths):
+            ax1 = plt.subplot2grid((2,1), (0,0))
+            ax2 = plt.subplot2grid((2,1), (1,0))
+                
+            for n in range(PC_count):
+                ax1.plot(xd, PCs[:,n], label='PC #' + str(n+1), color='C'+str(n + len(datas_lengths)))
+                ax1.set_xlabel(xlabel)
+                ax1.set_ylabel(ylabel)
+                ax1.legend(loc='upper right')
+                ax1.set_xlim([xd[0],xd[-1]])
+            current_data = 0
+            for length in range(len(datas_lengths)):
+                if length == 0:
+                    ax2.scatter(samples_PC_space[0,0:datas_lengths[0]], samples_PC_space[1,0:datas_lengths[0]] , edgecolors='k', alpha=0.1, color='C'+str(current_data ))
+                    print(str(0) + ',')
+                    print(str(datas_lengths[0]))
+                else:
+                    ax2.scatter(samples_PC_space[0,datas_lengths[length-1]:datas_lengths[length]], samples_PC_space[1,datas_lengths[length-1]:datas_lengths[length]] , edgecolors='k', alpha=0.2, color='C'+str(current_data ))
+                    print(str(datas_lengths[length-1]) + ',')
+                    print(str(datas_lengths[length]))
+                current_data+=1
+            ax2.set_xlabel('PC #1 (' + str(round(v[0]*100,2)) + ' %)')
+            ax2.set_ylabel('PC #2 (' + str(round(v[1]*100,2)) + ' %)')
+            ax2.set_xlim([np.min(samples_PC_space[0,:]) , np.max(samples_PC_space[0,:])])
+            ax2.set_ylim([np.min(samples_PC_space[1,:]) , np.max(samples_PC_space[1,:])])
+            ax1.set_title('PCA Results - ' + self.name + ' data') 
+            plt.tight_layout()
+        
+        selections = input('What would you like to plot?\nChoose from: X,X_norm,X_shifted,X_histograms,force_Curves,PCA\n')
+        selections = selections.split(',')
+        
+        
+        if 'force_Curves' in selections:
+            n=0
+            current_set = 0
+            for column in self.X_AFM_merged.T:
+                if n in self.datas_length: 
+                #this condition flags in which set we are
+                    if n==0:
+                        current_set=0
+                    else:
+                        current_set+=1
+                clr = 'C'+str(current_set)
+                plt.plot(self.all_objects[current_set].x_modified, column, lw=0.4, color=clr)
+                n+=1
+            plt.xlim(self.all_objects[0].x_modified[0],self.all_objects[0].x_modified[-1])
+            plt.title('All Force Curves')
+        if 'PCA' in selections:
+            typ = input('Which PCA would you like to plot?\nChoose from: data, data_norm, data_histograms, data_KDE, data_AFM\n')
+            typ = typ.split(',')
+            PC_count = input('How many PCs would you like to plot?\n')
+            PC_count = int(PC_count)
+            plt.figure(5)
+            if 'data_AFM' in typ:
+                plot_pca(self.all_objects[0].x_modified,PC_count, self.X_AFM_PCs, self.X_AFM_samples_PC_space, self.X_AFM_v, 'Position [m]', 'Force', self.datas_length)
+        
+
+    
+    def pca(self, shiftData=False):
+        #this function calculaation the principal components of the data
+        
+        typ = input('Which data would you like to calculate PCA for?\nChoose from: data,data_norm,data_histograms,data_KDE,data_forceCurves\n')
+        
+        if typ == 'data_norm':
+            data = self.X_norm
+        if typ == 'data':
+            data = self.X
+        if typ == 'data_histograms':
+            data = self.X_hist
+        if typ == 'data_KDE':
+            data =self.X_KDE
+        if typ == 'data_forceCurves':
+            data = self.X_AFM_merged
+        
+        [M,N] = data.shape
+        #If needed, substract the mean of each dimension (=sample type)
+        if shiftData:
+            data_mean = np.mean(data,1)
+            data = data - np.tile(data_mean, (N,1)).T
+            self.X_shifted = data
+        #calculate the coveriance/correlation matrix
+        # In order to avoid zero standard deviation check before if there are rows of zero
+        if True in (data == 0).all(axis=1):
+            data_covariance = np.cov(data)
+            print('Used covariance matrix\n')
+        else: #if there are not rows of zero, than there will be no dividing by zero
+            data_covariance = np.corrcoef(data)
+            print('Used correlation matrix\n')
+        if typ == 'data_forceCurves':
+            data_covariance = np.cov(data)
+            print('Used covariance matrix\n')
+        #find the eigenvalues and eigenvectors
+        #(using np.linalg.eigh function which assumes a real and symettric matrix)
+        [v,PCs] = np.linalg.eigh(data_covariance)
+        #normalizing the eigenvmaxectors to be in precntage
+        v = v / np.sum(v)
+        #sort the values in decreasing order
+        sorted_indexexs = np.argsort(-v) #get the indexes first
+        v   = v[sorted_indexexs]
+        PCs[:] = PCs[:,sorted_indexexs] 
+        #project the data set on the PCs space
+        samples_PC_space = np.matmul(PCs.T , data)
+        if typ == 'data_norm':
+            self.X_norm_covariance       = data_covariance
+            self.X_norm_v                = v
+            self.X_norm_PCs              = PCs
+            self.X_norm_samples_PC_space = samples_PC_space
+        if typ == 'data':
+            self.X_covariance       = data_covariance
+            self.X_v                = v
+            self.X_PCs              = PCs
+            self.X_samples_PC_space = samples_PC_space
+        if typ == 'data_histograms':
+            self.X_hist_covariance       = data_covariance
+            self.X_hist_v                = v
+            self.X_hist_PCs              = PCs
+            self.X_hist_samples_PC_space = samples_PC_space
+        if typ == 'data_KDE':
+            self.X_KDE_covariance       = data_covariance
+            self.X_KDE_v                = v
+            self.X_KDE_PCs              = PCs
+            self.X_KDE_samples_PC_space = samples_PC_space
+        if typ == 'data_forceCurves':
+            self.X_AFM_covariance       = data_covariance
+            self.X_AFM_v                = v
+            self.X_AFM_PCs              = PCs
+            self.X_AFM_samples_PC_space = samples_PC_space
+        
+        print('Finished computing PCA\n========================\n')
+        print('Variance of the first 5 principal components:\n')
+        print(v[0:5])
